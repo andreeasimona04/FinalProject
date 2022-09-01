71cd9264af66f40775eb9c446e3626456b1b3f82
andreeasimona2604 <andreeatelegeanu@gmail.com>
2022-09-01 17:33:22 +0300

    almost 2

--- API1.py
+++ API1.py
@@ -5,7 +5,7 @@ from secrets import API_KEY
 
 def get_email_content():
     try:
-        response = requests.get(f"https://newsapi.org/v2/everything?q=Apple&from=2022-08-20&sortBy=popularity&apiKey={API_KEY}")
+        response = requests.get(f"https://newsapi.org/v2/everything?q=IT&from=2022-08-25&pageSize=10&page=1&language=en&apiKey={API_KEY}")
     except requests.exceptions.RequestException as err:
         print(err)
     else:
@@ -16,7 +16,7 @@ def get_email_content():
 
     for i in data["articles"]:
         print(i["title"])
-        print(i["description"])
+        # print(i["description"])
         print(i["url"])
 
 print(get_email_content())
--- Email.py
+++ Email.py
@@ -5,37 +5,19 @@ from secrets import EMAIL_USER, EMAIL_PASS
 from API1 import get_email_content
 from db import count_emails, read_emails
 
-ROOT = pathlib.Path(__file__).parent
-DB_file = ROOT.joinpath("ForEmail.db")
-conn = sqlite3.connect(DB_file)
+# ROOT = pathlib.Path(__file__).parent
+# DB_file = ROOT.joinpath("ForEmail.db")
+# conn = sqlite3.connect(DB_file)
 
 gmail_user = EMAIL_USER
 gmail_pass = EMAIL_PASS
-to = read_emails(conn)
-subject = "Latest IT news!"
-body = get_email_content()
+gmail_server = "smtp.gmail.com"
+gmail_server_port = 465
 
-email_text = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (gmail_user, ", ".join(to), subject, body)
-
-try:
-    smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    smtp_server.ehlo()
-    smtp_server.login(gmail_user, gmail_pass)
-    smtp_server.sendmail(gmail_user, to, email_text)
-    smtp_server.close()
-    print("Email was sent.")
-except Exception as ex:
-    print("Something went wrong.", ex)
-
-
-
-# count_emails()
-
-
-
+server = smtplib.SMTP_SSL(gmail_server, gmail_server_port)
+server.login(gmail_user, gmail_pass)
+server.sendmail(
+    gmail_user,
+    "andreeatelegeanu@gmail.com",
+    "SEND"
+)
