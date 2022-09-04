e2b4ed921b5896e30f171a61b71b1b8f8ea37ae8
andreeasimona2604 <andreeatelegeanu@gmail.com>
2022-09-05 01:39:11 +0300

    almost 4

--- it_news.py
+++ it_news.py
@@ -2,14 +2,14 @@ import argparse
 import pathlib
 import sqlite3
 from db import add_new_user, delete_user
-from Email import send_email, email_adresses
+from Email import send_email
 
 ROOT = pathlib.Path(__file__).parent
 DB_file = ROOT.joinpath("ForEmail.db")
 conn = sqlite3.connect(DB_file)
 
 parser = argparse.ArgumentParser(description = "Modify DB.")
-parser.add_argument("email", type = str, help ="Email to delete or add.")
+parser.add_argument("-email", type = str, help ="Email to delete or add.")
 parser.add_argument("-d", action = "store_true", help = "Delete from DB.")
 parser.add_argument("-a", action = "store_true", help = "Add to DB.")
 parser.add_argument("-send", action = "store_true", help = "Send the email.")
@@ -22,7 +22,7 @@ if args.d:
     delete_user(conn, args.email)
 
 if args.send:
-    send_email(email_adresses)
+    send_email()
 
 
 
